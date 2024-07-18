a826572b99425b1185919106f143024c3c658c8d
Swathi-Kakunuri <swathireddyk666@gmail.com>
2024-07-18 10:04:11 +0530

    frontend project

---
+++ React projectworkspace/react-frontend/src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import  '../App.css';
+
+function FooterComponent() {
+  return (
+    <div>
+          <footer className='footer'>
+            <span>All Right Reserved 2024 &copy;SWATHI </span>
+          </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
